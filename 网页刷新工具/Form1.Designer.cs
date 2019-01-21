ac544dae23c0eea20f601cead4ea9171668623d2
Virink <virink@outlook.com>
2019-01-22 01:21:06 +0800

    add

---
+++ 网页刷新工具/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace 网页刷新工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.t_url = new System.Windows.Forms.TextBox();
+            this.siteurl = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.Label();
+            this.t_sec = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.stop = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.change = new System.Windows.Forms.Button();
+            this.logs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.06283F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(915, 480);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // t_url
+            // 
+            this.t_url.Font = new System.Drawing.Font("宋体", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.t_url.Location = new System.Drawing.Point(952, 78);
+            this.t_url.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.t_url.Name = "t_url";
+            this.t_url.Size = new System.Drawing.Size(459, 50);
+            this.t_url.TabIndex = 1;
+            // 
+            // siteurl
+            // 
+            this.siteurl.AutoSize = true;
+            this.siteurl.Font = new System.Drawing.Font("宋体", 13.94764F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.siteurl.Location = new System.Drawing.Point(945, 19);
+            this.siteurl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.siteurl.Name = "siteurl";
+            this.siteurl.Size = new System.Drawing.Size(93, 37);
+            this.siteurl.TabIndex = 2;
+            this.siteurl.Text = "网址";
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("宋体", 13.94764F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.time.Location = new System.Drawing.Point(945, 155);
+            this.time.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(169, 37);
+            this.time.TabIndex = 4;
+            this.time.Text = "刷新时间";
+            // 
+            // t_sec
+            // 
+            this.t_sec.Font = new System.Drawing.Font("宋体", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.t_sec.Location = new System.Drawing.Point(952, 216);
+            this.t_sec.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.t_sec.Name = "t_sec";
+            this.t_sec.Size = new System.Drawing.Size(459, 50);
+            this.t_sec.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.09424F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1093, 308);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 55);
+            this.label1.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1475, 1608);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1218, 32);
+            this.textBox3.TabIndex = 7;
+            // 
+            // stop
+            // 
+            this.stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.09424F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.Location = new System.Drawing.Point(958, 414);
+            this.stop.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(220, 79);
+            this.stop.TabIndex = 8;
+            this.stop.Text = "停止";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.09424F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(1204, 414);
+            this.start.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(207, 79);
+            this.start.TabIndex = 9;
+            this.start.Text = "启动";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // delete
+            // 
+            this.delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.09424F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete.Location = new System.Drawing.Point(958, 296);
+            this.delete.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(135, 79);
+            this.delete.TabIndex = 10;
+            this.delete.Text = "删除";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.09424F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(1114, 296);
+            this.add.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(136, 79);
+            this.add.TabIndex = 11;
+            this.add.Text = "新增";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // change
+            // 
+            this.change.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.09424F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.change.Location = new System.Drawing.Point(1277, 296);
+            this.change.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(134, 79);
+            this.change.TabIndex = 12;
+            this.change.Text = "确认";
+            this.change.UseVisualStyleBackColor = true;
+            this.change.Click += new System.EventHandler(this.change_Click);
+            // 
+            // logs
+            // 
+            this.logs.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logs.Location = new System.Drawing.Point(13, 500);
+            this.logs.Multiline = true;
+            this.logs.Name = "logs";
+            this.logs.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.logs.Size = new System.Drawing.Size(1398, 410);
+            this.logs.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1432, 922);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.change);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.t_sec);
+            this.Controls.Add(this.siteurl);
+            this.Controls.Add(this.t_url);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox t_url;
+        private System.Windows.Forms.Label siteurl;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.TextBox t_sec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button change;
+        private System.Windows.Forms.TextBox logs;
+    }
+}
+
---
+++ 网页刷新工具/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace 网页刷新工具
+{
+    public partial class Form1 : Form
+    {
+        string sitefile = "C:\\sites.json";
+        private Dictionary<string,System.Threading.Timer> _timer = new Dictionary<string, System.Threading.Timer>();
+        private SynchronizationContext mainThreadSynContext;
+        int timep = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.listView1.View = View.Details;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Columns.Add("Sec", 50, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("URL", 500, HorizontalAlignment.Left);
+            this.readSites();
+            mainThreadSynContext = SynchronizationContext.Current;
+        }
+
+        private void readSites()
+        {
+            try { 
+                StreamReader sr = new StreamReader(sitefile, Encoding.Default);
+                string sitejson = sr.ReadToEnd();
+                JArray jArray = (JArray)JsonConvert.DeserializeObject(sitejson);
+                Console.WriteLine(jArray);
+                if (jArray.Count > 0)
+                {
+                    for (int i = 0; i < jArray.Count; i++)
+                    {
+                        string sec = jArray[i]["Second"].ToString();
+                        string url = jArray[i]["Url"].ToString();
+                        this.addItem(sec, url);
+                    }
+                }
+                sr.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void writeSites()
+        {
+            ArrayList list1 = new ArrayList();
+            foreach (ListViewItem lvi in this.listView1.Items)
+            {
+                Site s = new Site { Second = lvi.SubItems[0].Text, Url = lvi.SubItems[1].Text };
+                list1.Add(s);
+            }
+            try { 
+                FileStream fs = new FileStream(sitefile, FileMode.OpenOrCreate);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(JsonConvert.SerializeObject(list1, Formatting.None));
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+                this.addLogs("Save sites to file success!");
+            }
+            catch(Exception eee)
+            {
+                Console.WriteLine(eee.ToString());
+            }
+        }
+
+        private void setContent(string sec, string url)
+        {
+            this.t_sec.Text = sec;
+            this.t_url.Text = url;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.listView1.SelectedItems.Count > 0)
+            {
+                try
+                {
+                    string sec = this.listView1.SelectedItems[0].SubItems[0].Text.ToString();
+                    string url = this.listView1.SelectedItems[0].SubItems[1].Text.ToString();
+                    this.setContent(sec, url);
+                }
+                catch (Exception ee)
+                {
+                    Console.WriteLine(ee.Message);
+                }
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem lvi in this.listView1.SelectedItems)
+            {
+                listView1.Items.RemoveAt(lvi.Index);
+            }
+            this.setContent("", "");
+            this.writeSites();
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            this.setContent("1", "");
+        }
+
+        private void addItem(string sec, string url)
+        {
+            ListViewItem[] p = new ListViewItem[1];
+            p[0] = new ListViewItem(new string[] { sec,url });
+            this.listView1.Items.AddRange(p);
+            this.addLogs("Add site ["+ url+"] success!");
+        }
+
+        private void change_Click(object sender, EventArgs e)
+        {
+            if (this.t_sec.Text != "" && this.t_url.Text != "") {
+                foreach (ListViewItem lvi in this.listView1.SelectedItems)
+                {
+                    listView1.Items.RemoveAt(lvi.Index);
+                }
+                this.addItem(this.t_sec.Text, this.t_url.Text);
+                this.setContent("1", "");
+            }
+            this.writeSites();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            /*
+            if (!this._timer.ContainsKey("clear"))
+            {
+                System.Threading.Timer _t = new System.Threading.Timer(OnClearLog, null, 0000, 60 * 1000);//2秒后第一次调用，每sec秒调用一次
+                this._timer.Add("clear", _t);
+            }
+            */
+            foreach (ListViewItem lvi in this.listView1.Items)
+            {
+                this.newTimer(int.Parse(lvi.SubItems[0].Text), lvi.SubItems[1].Text);
+            }
+        }
+
+        private void newTimer(int sec,string url)
+        {
+            if(!this._timer.ContainsKey(url)){ 
+                System.Threading.Timer _t = new System.Threading.Timer(httpGet, url, 2000 + timep, sec * 1000);//2秒后第一次调用，每sec秒调用一次
+                this._timer.Add(url, _t);
+                this.addLogs("Add Timer [" + url + "] success!");
+                timep += 1000;
+                Console.WriteLine(timep);
+            }
+        }
+
+        private void OnConnected(object state)
+        {
+            this.addLogs((string)state);
+        }
+
+        /*
+        private void OnClearLog(object state)
+        {
+            this.logs.Clear();
+        }
+        */
+
+        private void httpGet(object state)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = httpClient.GetAsync(new Uri((string)state)).Result;
+                string result = response.Content.ReadAsStringAsync().Result;
+                // this.addLogs("Req "+(string)state);
+                mainThreadSynContext.Post(new SendOrPostCallback(OnConnected), "Req " + (string)state + " -> "+result.Substring(0,500));
+            }
+        }
+
+        private void addLogs(string log)
+        {
+            this.logs.Text = string.Format("[+] {0} \r\n\r\n{1}\r\n", DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"), log);
+            //this.logs.AppendText();
+            //this.logs.Select(this.logs.TextLength, 0);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            foreach (var item in this._timer)
+            {
+                Console.WriteLine(item.Key);
+                item.Value.Change(-1,1);
+            }
+            this._timer.Clear();
+            timep = 0;
+        }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Site
+    {
+        [JsonProperty]
+        public string Url { get; set; }
+        [JsonProperty]
+        [DefaultValue(1)]
+        public string Second { get; set; }
+    }
+}
